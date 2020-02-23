1c54e030f4b5e65978772f4454f0251b2b51de76
Oleg Babich <krab.rakula@gmail.com>
2020-02-23 15:28:02 +0200

    major web-part fix

--- app.py
+++ app.py
@@ -50,24 +50,27 @@ def predict():
     
     if features['bmi'][0] > 25:
         recomendations.append('You may loose some weight')
+        
     if features['smoking'][0] == 3:
         recomendations.append('You may quit smiking')
-    if features['avg_glucose_level'][0] > 100:
-        recomendations.append('Visit your doctor to decrease your glycose level')
-    if features['hypertension'][0] == 1:
-        recomendations.append('Visit your doctor to control your blood pressure')
-    if features['heart_disease'][0] == 1:
-        recomendations.append('Visit your сardiologist at least once a year')
         
-    recomendations = "<br />".join(recomendations)
+    if (features['avg_glucose_level'][0] > 100) or (features['hypertension'][0] == 1) or (features['heart_disease'][0] == 1):
+        recomendations.append('Visit your doctor ')
+        
+    if features['avg_glucose_level'][0] > 100:
+        recomendations.append('to decrease your glycose level')
+    if features['hypertension'][0] == 1:
+        recomendations.append('to control your blood pressure')
+        
+    recomendations = "\n".join(recomendations)
     
     if prediction == 0:
-        return render_template('index.html', prediction_text=f'You are OK!')
+        return render_template('result.html', prediction_text="You are OK!")
     
     else: 
-        out = f'You are at risk! \n  Probability of stroke is {probability} \n To reduce the risk, please follow the next recommendations: <br /> {recomendations}'
+        out = f'You are at risk! \n  Probability of stroke is {probability} \n To reduce the risk, please follow the next recommendations: \n {recomendations}'
         
 
-    return render_template('index.html', prediction_text=out)
+    return render_template('result.html', prediction_text=out)
 if __name__ == "__main__":
     app.run()
